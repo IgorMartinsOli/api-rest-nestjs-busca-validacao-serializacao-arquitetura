382252a56bffeeaadb9546af0a925977ab84d3c4
Igor Martins <igrmartoli@gmail.com>
2023-06-07 11:00:37 -0300

    iniciando serialização

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,12 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 import { IsNomeDeUsuarioUnico } from "./is-nome-de-usuario-unico.validator";
+import { Exclude, Expose } from "class-transformer";
+
+class UsuarioSaidaDTO {
+
+}
+
+class UsuarioEntradaDTO {
+
+}
 
 @Injectable()
 export class Usuario {
     
     id: number;
 
+    @Expose({
+        name: 'username'
+    })
     @IsNomeDeUsuarioUnico({
         message: "nomeDeUsuario tem que ser unico"
     })
@@ -18,20 +30,35 @@ export class Usuario {
     })
     nomeDeUsuario: string;
 
+    @Expose({
+        name: 'email'
+    })
     @IsEmail({}, {
         message: 'email precisa ser um endereço de email válido.'
     })
     email: string;
 
+    @Expose({
+        name: 'password'
+    })
+    @Exclude({
+        toPlainOnly: true
+    })
     @IsNotEmpty({
         message: 'senha é obrigatório.'
     })
     senha: string;
 
+    @Expose({
+        name: 'fullname'
+    })
     @IsNotEmpty({
         message: 'nomeCompleto é obrigatório.'
     })
     nomeCompleto: string;
 
+    @Expose({
+        name: 'joinDate'
+    })
     dataDeEntrada: Date;
 }
